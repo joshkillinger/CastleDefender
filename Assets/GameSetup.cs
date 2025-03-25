e77c926201a6adab7581342f3371d676a10cfb61
Josh Killinger <josh.killinger@gmail.com>
2025-03-25 15:34:59 -0600

    Add jdkore packages

---
+++ Assets/GameSetup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using OOECS.Entity;
+using UnityEngine;
+
+namespace CastleDefender
+{
+    public class GameSetup : MonoBehaviour
+    {
+        private void Awake()
+        {
+            EntityManager.Reset();
+        }
+    }
+}
